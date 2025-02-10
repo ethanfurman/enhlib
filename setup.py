ee74d1a01fc50ad57f11a57adf0e005afedb8a65
Ethan Furman <ethan@stoneleaf.us>
2025-02-10 12:55:17 -0800

    version 0.0.4

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ Enhanced Library -- extras beyond Python's stdlib
 
 data = dict(
        name='enhlib',
-       version='0.0.4a1',
+       version='0.0.4',
        url='https://github.com/ethanfurman/enhlib',
        packages=['enhlib','enhlib.dbf','enhlib.stdlib','enhlib.test'],
        package_data={
