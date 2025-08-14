26c6a98ddacbf5207e3da19f32430de442fd091a
Ethan Furman <ethan@stoneleaf.us>
2025-08-14 11:38:27 -0700

    remove dbf

--- enhlib/datetime.py
+++ enhlib/datetime.py
@@ -1,8 +1,13 @@
 from .stdlib.datetime import date, time, datetime
-from .dbf import Date, Time, DateTime
 
-dates = date, Date
-times = time, Time
-datetimes = datetime, DateTime
+try:
+    from dbf import Date, Time, DateTime
+    dates = date, Date
+    times = time, Time
+    datetimes = datetime, DateTime
+except ImportError:
+    dates = date,
+    times = time,
+    datetimes = datetime,
 
 moments = dates + times + datetimes
